08f2749f0e110b40d0aecdfb2e7a69b9f5a702db
javierFili <javierfiligranaagreda@gmail.com>
2023-07-28 16:57:20 -0400

    el error del carrusel es por la version que es antigua, entonces voy a realizar otro carrusel

--- proyectoinviernofrontend/src/components/ViewCarouselHotel.jsx
+++ proyectoinviernofrontend/src/components/ViewCarouselHotel.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
+import { MDBCarousel, MDBCarouselItem, MDBContainer,MDBCardImage } from "mdb-react-ui-kit";
 // eslint-disable-next-line react/prop-types
-export default function ViewCarouselHotel({ imagenes,ancho, largo }) {
-  let num = 0;  
+export default function ViewCarouselHotel({ imagenes, ancho, largo }) {
+  let num = 0;
+  //let idItem=0
   const imgs = () => {
     // eslint-disable-next-line react/prop-types
     return imagenes.map((name) => (
       <MDBCarouselItem
         key={num++}
-        className="w-100 d-block"
-        itemId={num++}
-        src={name}
+        className="w-100 d-block "
+        itemId={num++}      
         width={ancho}
         height={largo}
         style={{ padding: "1em" }}
-      />
+      >
+        <MDBCardImage
+          src={name}
+          alt="Imagen del establecimiento"
+        />
+      </MDBCarouselItem>
     ));
   };
   return (
     <>
       <div>
-        <MDBCarousel showControls>{imgs()}</MDBCarousel>
+        <MDBContainer className="mt-5">
+          <MDBCarousel showControls>{imgs()}</MDBCarousel>{" "}
+        </MDBContainer>
       </div>
     </>
   );
