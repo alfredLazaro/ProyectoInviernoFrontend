e1ebe12b96a4b67113ff2a84dbadb8a539962afc
Erick M <maumbeltran@gmail.com>
2023-07-30 16:33:33 -0400

    Vista registrar restaurante reutilizando componentes

--- proyectoinviernofrontend/src/App.js
+++ proyectoinviernofrontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import React from 'react';
 import PrincipalHotel from "./pages/vistaInformacionHotel/PrincipalHotel";
 import FormAlojamiento from "./pages/registroAlojamiento/FormAlojamiento";
+import FormRestaurante from "./pages/registroRestaurante/FormRestaurante";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/infhotel" element={ <PrincipalHotel /> } />
         <Route path="/registrarAloj" element={ <FormAlojamiento /> } />
+        <Route path="/registrarRestaurante" element={<FormRestaurante />} /> 
       </Routes>
       </div>
     </>
---
+++ proyectoinviernofrontend/src/pages/registroRestaurante/FormRestaurante.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import NavBar from "../../components/NavBar";
+import "./FormRestaurante.css";
+import TextField from '../../components/Forms/TextField';
+import TextArea from '../../components/Forms/TextArea';
+import TimeField from '../../components/Forms/TimeField';
+//import React, { useState } from "react";
+// import TextField from "../../components/Forms/TextField";
+// import TextArea from "../../components/Forms/TextArea";
+// import TimeField from "../../components/Forms/TimeField";
+// import axios from "axios";
+
+function FormRestaurante(){
+    
+    return(
+        <div>
+            <NavBar/>
+            <div className="formContainer">
+                <h1>Registro nuevo restaurante</h1>
+                <form className="formPlace" onSubmit={null}>  
+                {/* create function  handleSubmit */}
+                   <h2>Formulario de registro</h2>
+                    <div className='row'>
+                        <div className='col m-3'>
+                            <TextField fieldName={"Nombre del restaurante"} inputName={"name"}/>
+                            <TextField fieldName={"Nombre de la ubicacion"} inputName={"locationName"}/>
+                            <TextArea fieldName={'Descripcion del restaurante'} inputName={'description'}/>
+                            <TextField fieldName={'Detalles del restaurante'} inputName={'details'}/>
+                        </div>
+                        <div className="col m-3">
+                            <TextField fieldName={"Ubicacion en mapa"} inputName={"locationMap"} />
+                            <div className='row'>
+                                <div className='col'>
+                                    <TimeField fieldName={"Hora de apertura"} inputName={"openingTime"}/>
+                                </div>
+                                <div className='col'>
+                                    <TimeField fieldName={"Hora de cierre"} inputName={"closingTime"}/>
+                                </div>
+                            </div>
+                            <div className='mb-2 mt-2'>
+                                <label className='form-label'>Imagenes del restaurante: </label>
+                                <input type='file' multiple onChange={null}/>  
+                                {/* create function loadImages */}
+                            </div>
+                            <button className='btn btn-primary' onClick={null}>Subir Imagen</button>
+                            <section className='imageSection'>
+                                <ul>
+                                    {/* {File.map(file => {return (<li key={file.name}> {file.name}</li>)})} */}
+                                </ul>
+                            </section>
+                        </div>
+                       
+                    </div>
+                        <div className='center'>
+                            <button className='btn btn-primary'>Completar registro</button>
+                            <button className='btn btn-primary' onClick={null}>Cancelar</button>
+                            {/* cancelForm */}
+                        </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default FormRestaurante
