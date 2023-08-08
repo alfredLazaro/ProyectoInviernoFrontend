02edb1d34bdaf95a8508e0750a42b52f2d1bc2cb
alfredLP555 <alfredolazaro999@gmail.com>
2023-08-07 21:34:41 -0400

    arreglo el app

--- proyectoinviernofrontend/src/App.js
+++ proyectoinviernofrontend/src/App.js
@@ -1,14 +1,11 @@
 import { Routes, Route } from "react-router-dom"
 import "./App.css";
 import React from 'react';
-<<<<<<< HEAD
 import PrincipalHotel from "./pages/viewInformationHotel/PrincipalHotel";
 import PrincipalListHoteles from "./pages/viewListHotes/PrincipalListHoles";
 import NavBar from "./components/NavBar";
-=======
-import PrincipalHotel from "./pages/vistaInformacionHotel/PrincipalHotel";
+/* import PrincipalHotel from "./pages/vistaInformacionHotel/PrincipalHotel"; */
 import FormAlojamiento from "./pages/registroAlojamiento/FormAlojamiento";
->>>>>>> US-1
 
 function App() {
   return (
@@ -17,11 +14,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/infhotel" element={ <PrincipalHotel /> } />
-<<<<<<< HEAD
         <Route path="/hotels" element={<PrincipalListHoteles/>}/>
-=======
         <Route path="/registrarAloj" element={ <FormAlojamiento /> } />
->>>>>>> US-1
       </Routes>
       </div>
     </>
