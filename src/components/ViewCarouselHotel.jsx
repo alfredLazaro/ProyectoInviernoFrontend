16f42113f38d18bf4038f3ca329e977ade5fbedb
alfredLP555 <alfredolazaro999@gmail.com>
2023-08-10 09:21:07 -0400

    esta es la ultima correccion espero funcione bien

--- src/components/ViewCarouselHotel.jsx
+++ src/components/ViewCarouselHotel.jsx
@@ -1,32 +1,4 @@
 import React from "react";
-import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
-// eslint-disable-next-line react/prop-types
-export default function ViewCarouselHotel({ imagenes,ancho, largo }) {
-  let num = 0;  
-  const imgs = () => {
-    // eslint-disable-next-line react/prop-types
-    return imagenes.map((name) => (
-      <MDBCarouselItem
-        key={num++}
-        className="w-100 d-block"
-        itemId={num++}
-        src={name}
-        width={ancho}
-        height={largo}
-        style={{ padding: "1em" }}
-      />
-    ));
-  };
-  return (
-    <>
-      <div>
-        <MDBCarousel showControls>{imgs()}</MDBCarousel>
-      </div>
-    </>
-  );
-}
-/* eslint-disable react/prop-types */
-import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
