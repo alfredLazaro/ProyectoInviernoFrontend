0d0a7019ca6b34015f545837be3cfd02b39a201e
alfredLP555 <alfredolazaro999@gmail.com>
2023-08-18 23:16:56 -0400

    ya funciona correctamente

--- src/components/carruselRest/ViewCarrucelRest.jsx
+++ src/components/carruselRest/ViewCarrucelRest.jsx
@@ -1,86 +1,53 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 import React from "react";
-/* import { useEffect } from "react"; */
-/* import { Carousel } from "react-responsive-carousel"; */
+import { useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
 import { useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 // eslint-disable-next-line react/prop-types
 export default function ViewCarouselHotel({ imagenes, ancho, largo }) {
-  //var ruta="http://localhost:8080/restaurant/";
-/*   const [idImg, setIdImg] = React.useState(null); */
-  
-    function imageDisplay() {
-        const [imageData, setImageData] = useState(null);
-        //const idImg=6;
+  const [imageData, setImageData] = useState(null);
+  imagenes = imagenes.map((id, index) => {
         const getImageFromServer = async () => {
-            try {
-              
-                  const response = await axios.get('http://localhost:8080/image/fileSystem/6', {
-                      responseType: 'arraybuffer', // Indica que la respuesta es un array de bytes
-                  });
-
-                  // Convertir el array de bytes en un Blob
-                  const blob = new Blob([response.data], { type: 'image/png' });
-
-                  // Convertir el Blob en una URL
-                  const imageUrl = URL.createObjectURL(blob);
-
-                  // Guardar la URL en el estado
-                  setImageData(imageUrl);
-
-                  // Guardar la URL en localStorage
-                  localStorage.setItem('imageData', imageUrl);
-              
-            } catch (error) {
-                console.error('no se cargo la imagen', error);
-            }
-        };
-
+          try {
+                const response = await axios.get(`http://localhost:8080/image/fileSystem/${id}`, {
+                    responseType: 'arraybuffer', // Indica que la respuesta es un array de bytes
+                });
+                // Convertir el array de bytes en un Blob
+                const blob = new Blob([response.data], { type: 'image/png' });
+                // Convertir el Blob en una URL
+                const imageUrl = URL.createObjectURL(blob);
+                // Guardar la URL en el estado
+                setImageData(imageUrl);
+                // Guardar la URL en localStorage
+                localStorage.setItem('imageData', imageUrl);
+          } catch (error) {
+              console.error('no se cargo la imagen', error);
+          }
+      };
+      useEffect(() => {
+        getImageFromServer(); // Llamamos a la función para obtener la imagen cuando el componente se monta
+      }, []);
         return (
-            <div>
-                <button onClick={getImageFromServer}>Obtener Imagen</button>
-                {imageData && <img src={imageData} alt="Image" />}
-            </div>
+          <div className="slide" key={index}>
+            <img
+              alt="sample_file"
+              src={imageData} 
+              key={index}
+              width={ancho}
+              height={largo}
+            />
+          </div>
         );
-    }
-  // eslint-disable-next-line react/prop-types
-  /* imagenes = imagenes.map((URL, index) => (
-    <div className="slide" key={index}>
-      <img
-        alt="sample_file"
-        src={URL}
-        key={index}
-        width={ancho}
-        height={largo}
-      />
-    </div>
-  )); */
-  /* // eslint-disable-next-line react/prop-types
-  imagenes = imagenes.map((URL, index) => {
-    let fixedURL = URL.replace(/\\/g, "/");
-    
-    return (
-      <div className="slide" key={index}>
-        <img
-          alt="sample_file"
-          src={fixedURL} 
-          key={index}
-          width={ancho}
-          height={largo}
-        />
-       <p>{fixedURL}</p>
-      </div>
-    );
-  }); */
-  
+  });
   return (
     <>
       <div>
         <div className="box">
-          {/* <Carousel  timer={2000} useKeyboardArrows={true} >{imagenes}</Carousel> */}
-          {imageDisplay()}
+          <Carousel  timer={2000} useKeyboardArrows={true} >{imagenes}</Carousel>
         </div>
       </div>
     </>
